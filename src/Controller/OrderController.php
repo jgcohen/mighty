1dc84c55bceaacc37b09b090e9880d15f3685111
jgcohen <75163282+jgcohen@users.noreply.github.com>
2021-09-24 22:31:51 +0200

    adding stripe

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,11 +2,11 @@
 
 namespace App\Controller;
 
+
 use App\Classe\Cart;
 use App\Entity\Order;
 use App\Entity\OrderDetails;
 use App\Form\OrderType;
-use DateTime;
 use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,19 +14,21 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 
+
 class OrderController extends AbstractController
 {
 
     private $entityManager;
 
-    public function __construct (EntityManagerInterface $entityManager){
+    public function __construct(EntityManagerInterface $entityManager)
+    {
         $this->entityManager = $entityManager;
     }
     /**
      * @Route("/commande", name="order")
      */
     public function index(Cart $cart): Response
-    
+
     {
         if (!$this->getUser()->getAddresses()->getValues()) {
             return $this->redirectToRoute('add_account_address');
@@ -63,15 +65,17 @@ class OrderController extends AbstractController
 
 
             $order = new Order();
+            $reference=$date->format('dmY').'-'.uniqid();
+            $order->setReference($reference);
             $order->setUser($this->getUser());
             $order->setCreatedAt($date);
             $order->setCarrierName($carriers->getName());
             $order->setCarrierPrice($carriers->getPrice());
             $order->setDelivery($delivery_content);
             $order->setIsPaid(0);
-
+            
             $this->entityManager->persist($order);
-
+        
             foreach ($cart->getFull() as $product) {
                 $orderDetails = new OrderDetails();
                 $orderDetails->setMyOrder($order);
@@ -81,13 +85,20 @@ class OrderController extends AbstractController
                 $orderDetails->setTotal($product['product']->getPrice() * $product['quantity']);
                 $this->entityManager->persist($orderDetails);
 
-                
             }
-            // $this->entityManager->flush();
+           
+             $this->entityManager->flush();
+
+           
+
+
+           
             return $this->render('order/add.html.twig', [
                 'cart' => $cart->getFull(),
-                'carrier'=>$carriers,
-                'delivery'=>$delivery_content
+                'carrier' => $carriers,
+                'delivery' => $delivery_content,
+                'reference'=>$order->getReference()
+           
             ]);
         }
         return $this->redirectToRoute('cart');
---
+++ src/Controller/StripeController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+
+use App\Classe\Cart;
+use App\Entity\Order;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StripeController extends AbstractController
+{
+    /**
+     * @Route("/commande/create-session/{reference}", name="stripe_create_session")
+     */
+    public function index(Cart $cart,$reference, EntityManagerInterface $entitymanager): Response
+    {
+
+        $product_for_stripe = [];
+        $YOUR_DOMAIN = 'http://localhost:8000';
+
+        $order = $entitymanager->getRepository(Order::class)->findOneBy(array('reference'=>$reference));
+       if(!$order){
+           return $this->redirectToRoute('cart');
+       }
+        foreach ($order->getOrderDetails()->getValues() as $product) {
+            $product_object = $entitymanager->getRepository(Product::class)->findOneBy(array('name'=>$product->getProduct()));
+            $product_for_stripe[] = [
+                'price_data' => [
+                    'currency' => 'eur',
+                    'unit_amount' => $product->getPrice(),
+                    'product_data' => [
+                        'name' => $product->getProduct(),
+                        'images' => [$YOUR_DOMAIN."/uploads/".$product_object->getIllustration()]
+                    ]
+                ],
+                'quantity' => $product->getQuantity(),
+            ];
+
+        }
+            $product_for_stripe[] = [
+                'price_data' => [
+                    'currency' => 'eur',
+                    'unit_amount' => $order->getCarrierPrice()*100,
+                    'product_data' => [
+                        'name' => $order->getCarrierName(),
+                        // 'images' => [$YOUR_DOMAIN."/uploads/".$product_object->getIllustration()]
+                    ]
+                ],
+                'quantity' => 1,
+            ];
+        \Stripe\Stripe::setApiKey('sk_test_51JdGLKLXl6JZC1riGvgqhA9BayMGQZMmlAQpP2lkJyXQU9saRoWlT0toGGvrOwnIXAA6UlWBEglduyy0T4r7zGWF00Ns9jZLzd');
+        $checkout_session = \Stripe\Checkout\Session::create([
+            'customer_email'=>$this->getUser()->getEmail(),
+            'line_items' => [$product_for_stripe],
+            'payment_method_types' => [
+                'card',
+            ],
+            'mode' => 'payment',
+            'success_url' => $YOUR_DOMAIN . '/success.html',
+            'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+        ]);
+        return $this->redirect($checkout_session->url,303);
+    }
+}
